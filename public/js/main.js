30264614fc4960c31361967e0d1b1fdb1df3f72a
Abhishek Shrestha <abhishekdata53@gmail.com>
2020-02-15 22:22:09 +0545

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
 // Make sure service worker are supported
-if('serviceWorker' in navigator){
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-        .register('../sw.js')
-        .then(reg => console.log('Service Worker: Registered'))
-        .catch(err => console.log(`Service Worker: Error: ${err}`))
-    })
-}
+// if('serviceWorker' in navigator){
+//     window.addEventListener('load', () => {
+//         navigator.serviceWorker
+//         .register('../sw.js')
+//         .then(reg => console.log('Service Worker: Registered'))
+//         .catch(err => console.log(`Service Worker: Error: ${err}`))
+//     })
+// }
